23355ac650a3c9cc63d52970208c55411ce3ad64
Саратова Екатерина Сергеевна <Ekaterina_Saratova@epam.com>
2018-10-13 14:48:51 +0400

    HOL2. Add runtime-specific code

--- src/EPAM.CoreWorkshop.ReportHelper/ReportNameHelper.cs
+++ src/EPAM.CoreWorkshop.ReportHelper/ReportNameHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace EPAM.CoreWorkshop.ReportHelper
 {
@@ -13,6 +14,21 @@ namespace EPAM.CoreWorkshop.ReportHelper
             }
 
             return name;
+        }
+
+        public static class ArrayHelper
+        {
+            public static void Fill<T>(T[] array, T value)
+            {
+#if NETCOREAPP2_0 || NETCOREAPP2_1
+                Array.Fill<T>(array, value);
+#else
+            for (var i = array.GetLowerBound(0); i <= array.GetUpperBound(0); i++)
+            {
+                array[i] = value;
+            }
+#endif
+            }
         }
     }
 }
